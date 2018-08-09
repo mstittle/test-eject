fe26a494ea7b41f5eef05b88a3b9ac57a7249313
mike stittleburg <mstittleburg@gmail.com>
2018-08-08 21:50:01 -0400

    test

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-export const x =      'x';
+export const x =     'x';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
