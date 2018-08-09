53bbacef9009537f0810aca53b49e93e3d95d941
mike stittleburg <mstittleburg@gmail.com>
2018-08-08 21:45:39 -0400

    x

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+export const x =       'x';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
